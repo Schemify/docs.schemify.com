f7f7ea2102381368dc843777a5d4a9fd79cdc7fc
ixyzdev <ixyz.dev@gmail.com>
2025-09-04 22:18:27 -0400

    chore(workflows): fixing baseurl missing /

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: "https://schemify.github.io/docs.schemify.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs.schemify.com",
+  baseUrl: "/docs.schemify.com/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
