76e736054eca212561985c0b5a56dede75bd85ec
ixyzdev <ixyz.dev@gmail.com>
2025-09-04 22:20:18 -0400

    chore(workflows): fixing url removing extra data

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,15 +16,15 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://your-docusaurus-site.example.com',
-  url: "https://schemify.github.io/docs.schemify.com/",
+  url: "https://schemify.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/docs.schemify.com/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "schemify", // Usually your GitHub org/user name.
+  projectName: "docs.schemify.com", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
